17fb81fe8ffd658eef83e9fa7bdc3dabb9be9caa
YAlmonkari <36625605+YAlmonkari@users.noreply.github.com>
2018-08-29 14:07:34 +0200

    Update jscript.js

--- jscript.js
+++ jscript.js
@@ -1,10 +1,21 @@
-    var app = new Vue({
-      el: '#app',
-      data: {
-        message: 'Hello Vue!'
-      }
-      function kolor(kolor)
-      {
-      document.bgColor=kolor;
-      }
-                      })
+function kolor(kolor)
+{
+document.bgColor=kolor;
+}
+var menu = ["opcja1", "opcja2", "opcja3", "opcja4"];
+var app = new Vue({
+  el: '#app',
+  data: {
+    message: menu
+  }
+})
+var app1 = new Vue({
+  el: '#app-1',
+  data: {
+    seen: false
+  }
+methods: {
+    1: function () {
+      seen = true
+    }
+})
