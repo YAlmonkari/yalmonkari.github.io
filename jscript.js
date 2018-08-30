6990b99d2dd14249ce678deb947c29c04735f3e6
YAlmonkari <36625605+YAlmonkari@users.noreply.github.com>
2018-08-30 13:30:41 +0200

    Update jscript.js

--- jscript.js
+++ jscript.js
@@ -2,12 +2,7 @@ var app = new Vue({
   el: '#app',
   data: {
     message: 'Hello Vue!'
-    rawHtml: '  <p>
-<input type="button" value="Przycisk zmiany koloru tła na zielony" onclick="kolor('green');">
-<input type="button" value="Przycisk zmiany koloru tła na biały" onclick="kolor('white');">
-    </p>' 
   }
-
                   })
   function kolor(kolor)
 {
