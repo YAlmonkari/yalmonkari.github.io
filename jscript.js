32c3e6aa09c1f32120b85226b5e5828643bd1f48
YAlmonkari <36625605+YAlmonkari@users.noreply.github.com>
2019-05-09 15:42:08 +0200

    Update jscript.js

--- jscript.js
+++ jscript.js
@@ -1,1 +1,59 @@
-
+    function kolor(kolor)
+{
+document.bgColor=kolor;
+}
+    document.addEventListener("DOMContentLoaded", 
+    ()=> {
+var app = new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello Vue!',
+    title: 'Strona załadowała się ' + new Date().toLocaleString()
+  }
+})
+var app2 = new Vue({
+  el: '#app-2',
+  data: {
+    seen: true
+  }
+Vue.component('todo-item', {
+  template: '\
+    <li>\
+      {{ title }}\
+      <button v-on:click="$emit(\'remove\')">Skasuj</button>\
+    </li>\
+  ',
+  props: ['title']
+})
+new Vue({
+  el: '#todo-list',
+  data: {
+    newTodoText: '',
+    todos: [
+      {
+        id: 1,
+        title: 'opcja1',
+      },
+      {
+        id: 2,
+        title: 'opcja2',
+      },
+      {
+        id: 3,
+        title: 'opcja3'
+      }
+    ],
+    nextTodoId: 4
+  },
+  methods: {
+    addNewTodo: function () {
+      this.todos.push({
+        id: this.nextTodoId++,
+        title: this.newTodoText
+      })
+      this.newTodoText = ''
+    }
+  }
+})
+})
+})}
