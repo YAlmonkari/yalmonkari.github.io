ffe86e610f2796ec359e3ce690aab84937a65b48
YAlmonkari <36625605+YAlmonkari@users.noreply.github.com>
2019-05-09 15:38:21 +0200

    Update jscript.js

--- jscript.js
+++ jscript.js
@@ -1,18 +1,1 @@
-function kolor(kolor)
-{
-document.bgColor=kolor;
-}
-function move() {
-    var elem = document.getElementById("myBar");
-    var width = 1;
-    var id = setInterval(frame, 1);
-    function frame() {
-        if (width >= 100) {
-            clearInterval(id);
-        } else {
-            width++;
-            elem.style.width = width + '%';
-            elem.innerHTML = width * 1 + '%';
-        }
-    }
-}
+
